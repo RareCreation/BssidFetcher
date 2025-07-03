f1e38437977609bbeabf73e68bad26c72a42aca2
RarchikCreation <chimmichurri.bad@gmail.com>
2025-07-03 23:56:16 +0300

    added ->   BssidFetcher.java -> Utility for parsing mac address wifi   Main.java -> Launching the main application   MainView.java -> Creating the Main Window   styles.css -> Application styles   VendorLookup.java -> Utility for getting information about mac address   WifiCardView.java -> Wifi Card   WifiInfo.java -> data model for storing Wi-Fi information   WifiService.java -> asynchronously receive information about the current Wi-Fi connection   WifiViewModel.java -> a layer between the UI and business logic (service)

---
+++ src/main/com/rare/wifivendor/utils/VendorLookup.java
@@ -0,0 +1,32 @@
+package com.rare.wifivendor.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class VendorLookup {
+
+    public static String getVendor(String mac) throws IOException {
+        String urlStr = "https://api.macvendors.com/" + mac;
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
+
+        int status = conn.getResponseCode();
+        if (status == 200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder content = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            return content.toString();
+        }
+        return "Vendor info not found";
+    }
+}
